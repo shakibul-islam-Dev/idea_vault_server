b059a83c6073e92484ee2e834b03f12f52de4e75
shakibul-islam-dev <ishakibul704@gmail.com>
2026-05-18 18:54:54 +0600

    Adding Basic MongoDb

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
-import express from "express";
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const PORT = process.env.URI || 3000;
+const mongoUri = process.env.MONGODB_URI;
+const PORT = process.env.PORT || 4000;
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const client = new MongoClient(mongoUri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen("PORT", () => {
+  console.log(`Server is running on Port ${PORT}`);
+});
