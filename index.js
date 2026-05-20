8f49c1efa5133b0558f1f7b2917ebb1244b2111e
shakibul-islam-dev <ishakibul704@gmail.com>
2026-05-20 18:27:49 +0600

    adding data

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const mongoUri = process.env.MONGODB_URI;
 const PORT = process.env.PORT || 4000;
+
+app.use(express.json());
+app.use(cors());
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
@@ -17,16 +21,36 @@ const client = new MongoClient(mongoUri, {
 async function run() {
   try {
     await client.connect();
+
+    const db = client.db("IdeaVault");
+    const dataBaseCollection = db.collection("IdeaVaults");
+    app.post("/api/idea", async (req, res) => {
+      const newIdea = req.body;
+      const result = await dataBaseCollection.insertOne(newIdea);
+      res.json(result);
+    });
+
+    app.get("/api/idea", async (req, res) => {
+      const result = await dataBaseCollection.find({}).toArray();
+
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!",
     );
-  } finally {
-    await client.close();
+  } catch (error) {
+    // Error console log kora lagbe jate bhul bujha jay
+    console.error("Database connection failed:", error);
   }
 }
 run().catch(console.dir);
 
-app.listen("PORT", () => {
+app.get("/", (req, res) => {
+  res.send("Server is running and Database is ready!");
+});
+
+app.listen(PORT, () => {
   console.log(`Server is running on Port ${PORT}`);
 });
