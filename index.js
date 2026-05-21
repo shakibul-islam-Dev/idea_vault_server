e483b9aaabb35685140d8e5b2a850ad5c2641561
shakibul-islam-dev <ishakibul704@gmail.com>
2026-05-21 21:12:06 +0600

    change the cors

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ const app = express();
 const mongoUri = process.env.MONGODB_URI;
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
-app.use(cors());
+const corsOptions = {
+  origin: "https://idea-vault-sooty.vercel.app",
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 const { createRemoteJWKSet, jwtVerify } = require("jose-cjs");
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
