82a1ee49c305e5ed108127b1fda7fee55dad0c67
shakibul-islam-dev <ishakibul704@gmail.com>
2026-05-22 23:18:41 +0600

    k

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ const mongoUri = process.env.MONGODB_URI;
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
 //CORS
-app.use(cors());
+app.use(
+  corscors({
+    origin: ["https://idea-vault-sooty.vercel.app"],
+    credentials: true,
+  }),
+);
 // JWT
 const { createRemoteJWKSet, jwtVerify } = require("jose-cjs");
 
